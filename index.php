7a41ca00b4a5f820b4602f50b14afaacfbb834b3
Alita <agniailma01@gmail.com>
2026-03-19 18:36:23 +0700

    update index dari laragon

--- index.php
+++ index.php
@@ -1,41 +1,53 @@
 <?php
+// Class induk untuk menyimpan data Sivitas Akademik
 class SivitasAkademik {
-    protected $nama;
+    protected $nama; // Property nama civitas akademik
 
+    // Constructor untuk inisialisasi nama
     public function __construct($nama) {
         $this->nama = $nama;
     }
 
+    // Getter untuk mendapatkan nama
     public function getNama() {
         return $this->nama;
     }
 }
 
+// Class Dosen merupakan turunan dari SivitasAkademik
 class Dosen extends SivitasAkademik {
-    public $nidn;
+    public $nidn; // Property NIDN dosen
 
+    // Constructor untuk inisialisasi nama dan NIDN
     public function __construct($nama, $nidn) {
-        parent::__construct($nama);
+        parent::__construct($nama); // Panggil constructor parent
         $this->nidn = $nidn;
     }
 }
 
+// Class Mahasiswa merupakan turunan dari SivitasAkademik
 class Mahasiswa extends SivitasAkademik {
-    public $nim;
+    public $nim; // Property NIM mahasiswa
 
+    // Constructor untuk inisialisasi nama dan NIM
     public function __construct($nama, $nim) {
-        parent::__construct($nama);
+        parent::__construct($nama); // Panggil constructor parent
         $this->nim = $nim;
     }
 }
 
-$d1 = new Dosen("Pak Andi", "001122");
-$m1 = new Mahasiswa("Alita", "2306700022");
+// Instansiasi object Dosen
+$d1 = new Dosen("Ajib Abdul Kholiq, S.Kom.", "012345"); // Ganti Nama sesuai data
 
+// Instansiasi object Mahasiswa
+$m1 = new Mahasiswa("Alita Agnia Ilma", "2306700022"); // Ganti NIM sesuai data
+
+// Menampilkan data Dosen
 echo "Dosen:<br>";
 echo "Nama: " . $d1->getNama() . "<br>";
 echo "NIDN: " . $d1->nidn . "<br><br>";
 
+// Menampilkan data Mahasiswa
 echo "Mahasiswa:<br>";
 echo "Nama: " . $m1->getNama() . "<br>";
 echo "NIM: " . $m1->nim;
